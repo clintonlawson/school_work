aaf0d9dd8f2cce2084d1e7ce74d54fbb320e69aa
clintonlawson <kainelawson@gmail.com>
2017-06-01 21:47:25 -0500

    school_work
    I am missing the rest, I must of not saved it and since the course is over I have lost the rest....

---
+++ Small_Group_Project/Small_Group_Project/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Small_Group_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            public static void display_inventory()
+            {
+
+
+            }
+
+        }
+    }
+}
